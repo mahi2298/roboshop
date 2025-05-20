bf1a6b0e9c82245f5b5cd9856a4156c561b7f06b
maheswari <mahi.sgs22@gmail.com>
2025-05-20 17:12:52 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -8,12 +8,12 @@ INSTANCE=("mongodb" "mysql" "redis" "rabbitMQ" "catalogue" "user" "cart" "shippi
 
 for instance in ${INSTANCE[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]")
-        if [ $instance != "frontend"]
-        then 
-            IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Instances[0].PrivateIpAddress" --output text)
-        else
-            IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Instances[0].PublicIpAddress" --output text)
-        fi
-        echo "$instance IP address is $IP"
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" ) --query "Instances[0].InstanceId" --output text        
+    if [ $instance != "frontend"]
+    then 
+       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Instances[0].PrivateIpAddress" --output text)
+    else
+       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Instances[0].PublicIpAddress" --output text)
+    fi
+    echo "$instance IP address is $IP"
 done
