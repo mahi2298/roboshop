174f88b723fcb9598899aa7ca15f1b0d6d99ba73
maheswari <mahi.sgs22@gmail.com>
2025-05-20 22:10:24 +0530

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -31,10 +31,10 @@ VALIDATE(){
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copying the Repo file"
 
-dnf install mongodb-org -y 
+dnf install mongodb-org -y &>>$LOG_FILE
 VALIDATE $? "Installing the mongodb"
 
-systemctl enable mongod
+systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "Enabling the mongodb"
 
 systemctl start mongod
@@ -43,5 +43,5 @@ VALIDATE $? "Starting the mongodb"
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Editing the Mongodb file for remote connection"
 
-systemctl restart mongod
+systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "Restarting the MONGODB File"
