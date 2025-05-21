74400889356a7d768dc56b4a5a2c484e77e86a72
maheswari <mahi.sgs22@gmail.com>
2025-05-21 21:41:03 +0530

    Roboshop

--- redis.sh
+++ redis.sh
@@ -38,6 +38,8 @@ VALIDATE $? "Enabling the redis"
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing the redis"
 
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c /protected-mode no' /etc/redis/redis.conf # if they are multiples expressions we need to use -e twice
+#sed -i 's/protected-mode:yes/protectedmode:no/g' /etc/redis/redis.conf
 systemctl restart redis &>>$LOG_FILE
 
 systemctl enable redis &>>$LOG_FILE
@@ -47,8 +49,7 @@ VALIDATE $? "Enabling the redis"
 systemctl start redis &>>$LOG_FILE
 VALIDATE $? "Starting the redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c /protected-mode no' /etc/redis/redis.conf # if they are multiples expressions we need to use -e twice
-#sed -i 's/protected-mode:yes/protectedmode:no/g' /etc/redis/redis.conf
+
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME-$START_TIME))
