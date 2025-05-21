8acc26cc5c29f1816b7ba2149631a1c3a6e1ddf1
maheswari <mahi.sgs22@gmail.com>
2025-05-21 14:46:34 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SECURITY_GROUP_ID="sg-07f28bb3d8e722233"
+SECURITY_GROUP_ID="sg-0744e1600df4e870e"
 HOSTED_ZONE_ID="Z07100922S6MUFRUIEXB8"
 DOMAIN_NAME="pavithra.fun"
 INSTANCE=("mongodb" "mysql" "redis" "rabbitMQ" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 
-for instance in ${INSTANCE[@]}
+for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)      
     if [ $instance != "frontend" ]
     then 
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance IP address is: $IP"
 
@@ -25,7 +27,7 @@ do
         ,"Changes": [{
          "Action"              : "CREATE"
         ,"ResourceRecordSet"  : {
-         "Name"                 : "'$instance'.'$DOMAIN_NAME'"
+         "Name"                 : "'$RECORD_NAME'"
          ,"Type"                : "A"
          ,"TTL"                 : 1
          ,"ResourceRecords"     : [{
