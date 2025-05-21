c1c21365670e67128998fdb440910f477e2b790f
maheswari <mahi.sgs22@gmail.com>
2025-05-21 21:50:56 +0530

    Roboshop

--- redis.sh
+++ redis.sh
@@ -38,7 +38,7 @@ VALIDATE $? "Enabling the redis"
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing the redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c /protected-mode no' /etc/redis/redis.conf # if they are multiples expressions we need to use -e twice
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf # if they are multiples expressions we need to use -e twice
 #sed -i 's/protected-mode:yes/protectedmode:no/g' /etc/redis/redis.conf
 systemctl restart redis &>>$LOG_FILE
 
